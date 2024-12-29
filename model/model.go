45ebe8ddf2f7d97d4fe7a13d46c170f9e721222d
archeopternix <andreas.eisner@kouri.cc>
2024-12-29 19:51:30 +0100

    Add settings and name

--- model/model.go
+++ model/model.go
@@ -8,20 +8,53 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-
 // Model holds all entities, relations and lookups and is able to persist them as a YAML file
 type Model struct {
+	Name string
+	Settings
 	Entities  map[string]Entity
 	Relations map[string]Relation
-  Lookups map[string]Lookup
+	Lookups   map[string]Lookup
+}
+
+// Settings is the definition of the global attributes
+type Settings struct {
+	CurrencySymbol    string `yaml:"currency_symbol"`
+	DecimalSeparator  string `yaml:"decimal_separator"`
+	ThousendSeparator string `yaml:"thousend_separator"`
+	TimeFormat        string `yaml:"time_format"`
+	DateFormat        string `yaml:"date_format"`
+}
+
+func NewEUROSettings() Settings {
+	s := Settings{
+		CurrencySymbol:    `€`,
+		DecimalSeparator:  `,`,
+		ThousendSeparator: `.`,
+		TimeFormat:        "15:04:05",
+		DateFormat:        "02.01.2006",
+	}
+	return s
+}
+
+func NewUSSettings() Settings {
+	s := Settings{
+		CurrencySymbol:    `$`,
+		DecimalSeparator:  `.`,
+		ThousendSeparator: `,`,
+		TimeFormat:        "15:04:05",
+		DateFormat:        "01/02/2006",
+	}
+	return s
 }
 
 // NewModel creates a new model and initialize the maps used for entities, relations and lookups
 func NewModel() *Model {
 	m := new(Model)
+	m.Settings = NewEUROSettings()
 	m.Entities = make(map[string]Entity)
 	m.Relations = make(map[string]Relation)
-  m.Lookups = make(map[string]Lookup)
+	m.Lookups = make(map[string]Lookup)
 	return m
 }
 
@@ -58,13 +91,13 @@ func (m *Model) WriteYAML(writer io.Writer) error {
 	return nil
 }
 
-// EntityInRealtions checks if the entityname is contained in Source 
+// EntityInRealtions checks if the entityname is contained in Source
 // or Destination of one of the entities
 func (m Model) EntityInRealtions(entityname string) bool {
-  for _, r:= range m.Relations {
-    if r.ContainsEntity(entityname) {
-      return true
-    }
-  }
-  return false
+	for _, r := range m.Relations {
+		if r.ContainsEntity(entityname) {
+			return true
+		}
+	}
+	return false
 }
