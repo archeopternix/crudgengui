42d56a2ffb1a8ce0c0fd4b9a89e62528f44f3fe7
archeopternix <andreas.eisner@kouri.cc>
2025-01-06 18:23:27 +0100

    Bugfixes in testcases

--- go.mod
+++ go.mod
@@ -6,15 +6,18 @@ require (
 	github.com/gertd/go-pluralize v0.2.1
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/labstack/echo/v4 v4.10.2
+	github.com/stretchr/testify v1.8.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
--- internal/template_test.go
+++ internal/template_test.go
@@ -1,74 +1,60 @@
 package internal
 
 import (
-    "bytes"
-    "html/template"
-    "testing"
+	"bytes"
+	"strings"
+	"testing"
 
-    "github.com/labstack/echo/v4"
-    "github.com/stretchr/testify/assert"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
-// Mock echo.Context for testing purposes
-type MockContext struct{}
-
-func (mc *MockContext) Deadline() (deadline time.Time, ok bool) {
-    return time.Time{}, false
-}
-func (mc *MockContext) Done() <-chan struct{} {
-    return nil
-}
-func (mc *MockContext) Err() error {
-    return nil
-}
-func (mc *MockContext) Value(key interface{}) interface{} {
-    return nil
-}
-
 func TestNewTemplateRegistry(t *testing.T) {
-    tr := NewTemplateRegistry()
-    assert.NotNil(t, tr)
-    assert.NotNil(t, tr.templates)
-    assert.NotNil(t, tr.funcMap)
+	tr := NewTemplateRegistry()
+	assert.NotNil(t, tr)
+	assert.NotNil(t, tr.templates)
+	assert.NotNil(t, tr.funcMap)
 }
 
 func TestGetFuncMap(t *testing.T) {
-    tr := NewTemplateRegistry()
-    funcMap := tr.getFuncMap()
-    assert.NotNil(t, funcMap)
-    assert.Equal(t, strings.Title("hello"), funcMap["title"].(func(string) string)("hello"))
-    assert.Equal(t, strings.ToLower("HELLO"), funcMap["lowercase"].(func(string) string)("HELLO"))
-    assert.Equal(t, strings.ToUpper("hello"), funcMap["uppercase"].(func(string) string)("hello"))
+	tr := NewTemplateRegistry()
+	funcMap := tr.getFuncMap()
+	assert.NotNil(t, funcMap)
+	assert.Equal(t, strings.Title("hello"), funcMap["title"].(func(string) string)("hello"))
+	assert.Equal(t, strings.ToLower("HELLO"), funcMap["lowercase"].(func(string) string)("HELLO"))
+	assert.Equal(t, strings.ToUpper("hello"), funcMap["uppercase"].(func(string) string)("hello"))
 }
 
 func TestAddTemplate(t *testing.T) {
-    tr := NewTemplateRegistry()
-    err := tr.addTemplate("base", "", "testdata/base.html")
-    assert.NoError(t, err)
-    assert.Contains(t, tr.templates, "base")
+	tr := NewTemplateRegistry()
+	err := tr.addTemplate("base", "", "testdata/base.html")
+	assert.NoError(t, err)
+	assert.Contains(t, tr.templates, "base")
 
-    err = tr.addTemplate("child", "base", "testdata/child.html")
-    assert.NoError(t, err)
-    assert.Contains(t, tr.templates, "child")
+	err = tr.addTemplate("child", "base", "testdata/child.html")
+	assert.NoError(t, err)
+	assert.Contains(t, tr.templates, "child")
 }
 
 func TestAddTemplateOrPanic(t *testing.T) {
-    tr := NewTemplateRegistry()
-    assert.NotPanics(t, func() {
-        tr.AddTemplateOrPanic("base", "", "testdata/base.html")
-    })
-    assert.Panics(t, func() {
-        tr.AddTemplateOrPanic("", "", "testdata/base.html")
-    })
+	tr := NewTemplateRegistry()
+	assert.NotPanics(t, func() {
+		tr.AddTemplateOrPanic("base", "", "testdata/base.html")
+	})
+	assert.Panics(t, func() {
+		tr.AddTemplateOrPanic("", "", "testdata/base.html")
+	})
 }
 
 func TestRender(t *testing.T) {
-    tr := NewTemplateRegistry()
-    tr.AddTemplateOrPanic("base", "", "testdata/base.html")
-    tr.AddTemplateOrPanic("child", "base", "testdata/child.html")
+	e := new(echo.Echo)
 
-    var buf bytes.Buffer
-    err := tr.Render(&buf, "child", map[string]interface{}{"Name": "World"}, &MockContext{})
-    assert.NoError(t, err)
-    assert.Contains(t, buf.String(), "Hello, World!")
+	tr := NewTemplateRegistry()
+	tr.AddTemplateOrPanic("base", "", "testdata/base.html")
+	tr.AddTemplateOrPanic("child", "base", "testdata/child.html")
+
+	var buf bytes.Buffer
+	err := tr.Render(&buf, "child", map[string]interface{}{"Name": "World"}, e.AcquireContext())
+	assert.NoError(t, err)
+	assert.Contains(t, buf.String(), "Hello, World!")
 }
