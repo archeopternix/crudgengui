ae1d5e1a3e96f24a63e37183bcade6da31a8cc8a
archeopternix <39010055+archeopternix@users.noreply.github.com>
2025-01-06 15:45:24 +0100

    Update yamlmodel.go

--- internal/repository/yamlmodel.go
+++ internal/repository/yamlmodel.go
@@ -1,40 +1,42 @@
 package repository
 
 import (
-	model "crudgengui/model"
-	"os"
+    "crudgengui/model"
+    "os"
 )
 
+// YAMLModel struct represents a model for handling YAML file operations.
 type YAMLModel struct {
-	yamlfile string
+    yamlfile string
 }
 
+// NewYAMLModel creates a new instance of YAMLModel with the provided file name.
 func NewYAMLModel(fname string) *YAMLModel {
-	ym := new(YAMLModel)
-	ym.yamlfile = fname
-	return ym
+    return &YAMLModel{yamlfile: fname}
 }
 
+// WriteModel writes the given model to the YAML file.
 func (ym YAMLModel) WriteModel(m *model.Model) error {
-	file, err := os.Create(ym.yamlfile)
-	if err != nil {
-		return err
-	}
-	err = m.WriteYAML(file)
-	if err != nil {
-		return err
-	}
-	return nil
+    // Create a new file with the name stored in the YAMLModel struct.
+    file, err := os.Create(ym.yamlfile)
+    if err != nil {
+        return err
+    }
+    defer file.Close() // Ensure file is closed after writing
+
+    // Write the model data to the file in YAML format.
+    return m.WriteYAML(file)
 }
 
-func (ym *YAMLModel) ReadModel(m *model.Model) (err error) {
-	file, err := os.Open(ym.yamlfile)
-	if err != nil {
-		return err
-	}
-	err = m.ReadYAML(file)
-	if err != nil {
-		return err
-	}
-	return nil
+// ReadModel reads the model from the YAML file and populates the given model instance.
+func (ym *YAMLModel) ReadModel(m *model.Model) error {
+    // Open the file with the name stored in the YAMLModel struct.
+    file, err := os.Open(ym.yamlfile)
+    if err != nil {
+        return err
+    }
+    defer file.Close() // Ensure file is closed after reading
+
+    // Read the YAML data from the file and populate the model instance.
+    return m.ReadYAML(file)
 }
